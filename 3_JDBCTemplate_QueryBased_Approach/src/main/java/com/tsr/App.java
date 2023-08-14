2c65f6950468c89769d6bf1858fadb89d700a6e1
sairam566 <sairam1431995@gmail.com>
2023-08-14 11:19:44 +0530

    Added couple of methods

--- 3_JDBCTemplate_QueryBased_Approach/src/main/java/com/tsr/App.java
+++ 3_JDBCTemplate_QueryBased_Approach/src/main/java/com/tsr/App.java
@@ -3,6 +3,7 @@ package com.tsr;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.tsr.bo.ProductBO;
 import com.tsr.config.JavaConfig;
 import com.tsr.dao.ProductsDAO;
 
@@ -12,6 +13,16 @@ public class App
     {
     	ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
     	ProductsDAO dao = context.getBean("dao", ProductsDAO.class);
-    	System.out.println(dao.getNoOfProducts());
+    	//System.out.println("No.Of Products Count : "+dao.getNoOfProducts());
+    	//System.out.println("Max Amount in Products : "+dao.maxProductAmount());
+    	//System.out.println("Product Name : "+dao.getProductNameById(3));
+    	//System.out.println("Product Details : "+dao.getProductById(3));
+    	//System.out.println("Product Greater than 1LK : "+dao.getProductsGreaterThanGivenPrice(100000.0));
+    	//System.out.println("Product Greater than 80000K : "+dao.getProductsByPrice(80000.0));
+    	ProductBO bo = new ProductBO();
+    	bo.setpId(4);
+    	bo.setpName("IMac");
+    	bo.setpPrice(120400.56);
+    	System.out.println("Priduct Insert Status : "+dao.saveProduct(bo));
     }
 }
--- 3_JDBCTemplate_QueryBased_Approach/src/main/java/com/tsr/dao/ProductsDAO.java
+++ 3_JDBCTemplate_QueryBased_Approach/src/main/java/com/tsr/dao/ProductsDAO.java
@@ -1,24 +1,97 @@
 package com.tsr.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.tsr.bo.ProductBO;
 
 @Repository("dao")
 public class ProductsDAO {
 	
 	private final String SQL_NO_OF_PRODUCTS = "select count(1) from Products";
+	private final String SQL_MAX_PRODUCT_AMOUNT = "select max(price) from products";
+	private final String SQL_GET_PRODUCT_NM_BY_PRODUCT_ID = "select name from products where id = ?";
+	private final String SQL_GET_PRODUCT_BY_ID="select id, name, price from products where id = ?";
+	private final String SQL_PRODUCTS_GREATER_THEN_GN_PRICE = "select id, name, price from products where price > ?";
+	private final String SQL_GET_PRODUCTS_BY_PRICE = "select name, price from products where price >= ?";
+	private final String SQL_INSERT_PRODUCT = "insert into PRODUCTS(id, name, price) values(?,?,?)";
 	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
 	public ProductsDAO(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
-		System.out.println("called");
 	}
+	
+	/*-----------------------------------------------
+	 * Query's Returning Single Column
+	 * ----------------------------------------------
+	 */
 	
 	public int getNoOfProducts() {
 		return jdbcTemplate.queryForObject(SQL_NO_OF_PRODUCTS, Integer.class);
+	}
+	
+	public float maxProductAmount() {
+		return jdbcTemplate.queryForObject(SQL_MAX_PRODUCT_AMOUNT, Float.class);
+	}
+	
+	public String getProductNameById(int productId) {
+		return jdbcTemplate.queryForObject(SQL_GET_PRODUCT_NM_BY_PRODUCT_ID, String.class,productId);
+	}
+	
+	/*-----------------------------------------------
+	 * Query's Returning Multiple Column
+	 * ----------------------------------------------
+	 */
+	
+	public ProductBO getProductById(int productId) {
+		return jdbcTemplate.queryForObject(SQL_GET_PRODUCT_BY_ID, new CustomRowMapper(),productId);
+	}
+	
+	class CustomRowMapper implements RowMapper<ProductBO>{
+		@Override
+		public ProductBO mapRow(ResultSet rs, int rowNum) throws SQLException {
+			ProductBO bo = new ProductBO();
+			bo.setpId(rs.getInt(1));
+			bo.setpName(rs.getString(2));
+			bo.setpPrice(rs.getDouble(3));
+			return bo;
+		}
+	}
+	
+	public List<ProductBO> getProductsGreaterThanGivenPrice(Double price){
+		return jdbcTemplate.query(SQL_PRODUCTS_GREATER_THEN_GN_PRICE, (rs,rowNo)->{
+			ProductBO bo = new ProductBO();
+			bo.setpId(rs.getInt(1));
+			bo.setpName(rs.getString(2));
+			bo.setpPrice(rs.getDouble(3));
+			return bo;
+		},price);
+	}
+	
+	/*-----------------------------------------------
+	 * Query's Returning arbitrary number of Column
+	 * ----------------------------------------------
+	 */
+	
+	public List<Map<String, Object>> getProductsByPrice(Double price) {
+		return jdbcTemplate.queryForList(SQL_GET_PRODUCTS_BY_PRICE, price);
+	}
+	/*-----------------------------------------------
+	 * Insert Query
+	 * ----------------------------------------------
+	 */
+	
+	public int saveProduct(ProductBO bo) {
+		return jdbcTemplate.update(SQL_INSERT_PRODUCT, bo.getpId(), bo.getpName(), bo.getpPrice());
 	}
 	
 }
